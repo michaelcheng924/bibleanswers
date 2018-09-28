aaa873326a46d5365230f6be5c376facb43c28ea
Michael Cheng <cheng.c.mike@gmail.com>
2018-09-28 13:31:51 -0500

    add nicene

--- src/constants/posts/explaining/historicalDocuments/index.js
+++ src/constants/posts/explaining/historicalDocuments/index.js
@@ -1,11 +1,18 @@
 import apostlesCreed from "./apostlesCreed";
+import niceneCreed from "./niceneCreed";
 import chalcedonianCreed from "./chalcedonianCreed";
 import athanasianCreed from "./athanasianCreed";
 import thirtyNineArticles from "./thirtyNineArticles";
 
 const historicalDocuments = {
   subcategory: "Historical documents",
-  posts: [apostlesCreed, chalcedonianCreed, athanasianCreed, thirtyNineArticles]
+  posts: [
+    apostlesCreed,
+    niceneCreed,
+    chalcedonianCreed,
+    athanasianCreed,
+    thirtyNineArticles
+  ]
 };
 
 export default historicalDocuments;
--- src/constants/posts/explaining/historicalDocuments/niceneCreed.js
+++ src/constants/posts/explaining/historicalDocuments/niceneCreed.js
@@ -1,48 +1,52 @@
-import React from "react";
-
-import { View } from "react-native";
-import { Txt } from "../../components/shared";
-
-const NICENE_CREED = (
-  <View>
-    <Txt first>
+const content = `
+  <div class="writing">
+    <p>
       We believe in one God, the Father Almighty, Maker of heaven and earth, and
       of all things visible and invisible;
-    </Txt>
-    <Txt>
+    </p>
+    <p>
       And in one Lord Jesus Christ, the Son of God, the Only-begotten, Begotten
       of the Father before all ages, Light of Light, Very God of Very God,
       Begotten, not made; of one essence with the Father,
-    </Txt>
-    <Txt>by whom all things were made;</Txt>
-    <Txt>
+    </p>
+    <p>by whom all things were made;</p>
+    <p>
       Who for us men and for our salvation came down from heaven, and was
       incarnate of the Holy Spirit and the Virgin Mary, and was made man;
-    </Txt>
-    <Txt>
+    </p>
+    <p>
       And was crucified also for us under Pontius Pilate, and suffered and was
       buried; And the third day He rose again, according to the Scriptures;
-    </Txt>
-    <Txt>
+    </p>
+    <p>
       And ascended into heaven, and sits at the right hand of the Father;
-    </Txt>
-    <Txt>
+    </p>
+    <p>
       And He shall come again with glory to judge the living and the dead, Whose
       kingdom shall have no end.
-    </Txt>
-    <Txt>
+    </p>
+    <p>
       And we believe in the Holy Spirit, the Lord, and Giver of Life, Who
       proceeds from the Father, Who with the Father and the Son together is
       worshipped and glorified, Who spoke by the Prophets;
-    </Txt>
-    <Txt>And we believe in one, holy, catholic*, and apostolic Church.</Txt>
-    <Txt>We acknowledge one Baptism for the remission of sins.</Txt>
-    <Txt>We look for the Resurrection of the dead,</Txt>
-    <Txt>And the Life of the age to come. Amen.</Txt>
-    <Txt style={{ fontStyle: "italic" }}>
-      * that is, the true Christian church of all times and all places
-    </Txt>
-  </View>
-);
+    </p>
+    <p>And we believe in one, holy, catholic*, and apostolic Church.</p>
+    <p>We acknowledge one Baptism for the remission of sins.</p>
+    <p>We look for the Resurrection of the dead,</p>
+    <p>And the Life of the age to come. Amen.</p>
+    <p><em>* that is, the true Christian church of all times and all places</em></p>
+  </div>
+`;
 
-export default NICENE_CREED;
+const post = {
+  title: "The Nicene Creed",
+  subtitle:
+    "A statement of the orthodox faith of the early Christian church in opposition to certain heresies, especially Arianism",
+  imageUrl: "/images/nicene-creed.png",
+  url: "/answers/documents/nicene-creed",
+  added: "9/28/18",
+  tags: ["historical documents", "nicene creed"],
+  content
+};
+
+export default post;
