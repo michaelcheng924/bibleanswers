48e7cf09005bc42e29c4752e5b2a72b0328178b4
Michael Cheng <cheng.c.mike@gmail.com>
2018-09-30 16:38:28 -0500

    update for firestore

--- src/server/db.js
+++ src/server/db.js
@@ -7,7 +7,7 @@ const serviceAccount = {
   type: process.env.FIRESTORE_TYPE,
   project_id: process.env.FIRESTORE_PROJECT_ID,
   private_key_id: process.env.FIRESTORE_PRIVATE_KEY_ID,
-  private_key: process.env.FIRESTORE_PRIVATE_KEY,
+  private_key: process.env.FIRESTORE_PRIVATE_KEY.replace(/\\n/g, "\n"),
   client_email: process.env.FIRESTORE_CLIENT_EMAIL,
   client_id: process.env.FIRESTORE_CLIENT_ID,
   auth_uri: process.env.FIRESTORE_AUTH_URI,
@@ -17,12 +17,9 @@ const serviceAccount = {
   client_x509_cert_url: process.env.FIRESTORE_CLIENT_X509_CERT_URL
 };
 
-admin.initializeApp(
-  {
-    credential: admin.credential.cert(serviceAccount)
-  },
-  "bible-answers"
-);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 const db = admin.firestore();
 
